c61b71c77a14ded1d311b0b524eb94705a56b4d1
hacknugithub <manu_pacu@hotmail.com>
2022-10-28 03:26:26 -0500

    open official site in webview

--- app/src/main/java/com/manuelguerrero/listtvshows/activities/ShowTvDetailActivity.kt
+++ app/src/main/java/com/manuelguerrero/listtvshows/activities/ShowTvDetailActivity.kt
@@ -1,12 +1,19 @@
 package com.manuelguerrero.listtvshows.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Html
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.View.VISIBLE
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
@@ -21,6 +28,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class ShowTvDetailActivity : AppCompatActivity() {
     var ivDetailImage: ImageView? = null
     var tvDetailTitle: TextView? = null
@@ -30,6 +38,8 @@ class ShowTvDetailActivity : AppCompatActivity() {
     var tvShowDetail: TextView? = null
     var tvShowGenre: TextView? = null
     var tvShowSchedule: TextView? = null
+    var webView: WebView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +55,7 @@ class ShowTvDetailActivity : AppCompatActivity() {
         tvShowDetail = findViewById(R.id.tvShowDetail)
         tvShowGenre = findViewById(R.id.tvShowGenre)
         tvShowSchedule = findViewById(R.id.tvShowSchedule)
+        webView = findViewById(R.id.wvSite)
 
         bundle?.apply {
             val showId: String? = getString("showId")
@@ -55,8 +66,6 @@ class ShowTvDetailActivity : AppCompatActivity() {
                 loadShowCast(castRecyclerView, showId)
             }
         }
-
-
     }
 
     private fun loadShow(showId: String?){
@@ -65,37 +74,76 @@ class ShowTvDetailActivity : AppCompatActivity() {
         val requestCall = destinationService.getSingleShow(showId.toString())
 
         requestCall.enqueue(object : Callback<MySchedule.Show> {
-            override fun onResponse(call: Call<MySchedule.Show>, response: Response<MySchedule.Show>) {
+            @SuppressLint("SetJavaScriptEnabled")
+            override fun onResponse(
+                call: Call<MySchedule.Show>,
+                response: Response<MySchedule.Show>
+            ) {
                 Log.d("Response", "Show onResponse: ${response.body()}")
-                if (response.isSuccessful){
+                if (response.isSuccessful) {
                     val showItem = response.body()!!
                     val res = baseContext.resources
                     Picasso.get().load(showItem.image?.medium).fit().into(ivDetailImage)
                     tvDetailTitle?.text = showItem.name
                     tvNetwork?.text = showItem.network?.name
-                    tvDetailRating?.text = String.format(res.getString(R.string.tv_rating_detail),
+                    tvDetailRating?.text = String.format(
+                        res.getString(R.string.tv_rating_detail),
                         showItem.rating.average
                     )
                     val summarySanitized = Html.fromHtml(showItem.summary).toString()
-                    tvShowDetail?.text = String.format(res.getString(R.string.title_activity_tv_show_detail), summarySanitized)
-                    tvShowGenre?.text = String.format(res.getString(R.string.tv_genre_detail),
+                    tvShowDetail?.text = String.format(
+                        res.getString(R.string.title_activity_tv_show_detail),
+                        summarySanitized
+                    )
+                    tvShowGenre?.text = String.format(
+                        res.getString(R.string.tv_genre_detail),
                         showItem.genres.joinToString()
                     )
-                    tvShowSchedule?.text = String.format(res.getString(R.string.tv_schedule_detail), showItem.schedule.time,
+                    tvShowSchedule?.text = String.format(
+                        res.getString(R.string.tv_schedule_detail), showItem.schedule.time,
                         showItem.schedule.days.joinToString()
                     )
 
+
+                    val btnMaterial: MaterialButton = findViewById(R.id.tvSiteBtn)
+                    btnMaterial.setOnClickListener(fun(it: View) {
+                        val webSettings: WebSettings = webView!!.settings
+                        webSettings.javaScriptEnabled = true
+                        val baseUrl: String = showItem.officialSite.toString()
+                        webView!!.webViewClient = WebViewClient()
+                        webView!!.visibility = VISIBLE
+                        webView!!.bringToFront()
+                        showItem.officialSite?.let { this@ShowTvDetailActivity.webView!!.loadUrl(it) }
+                    })
+
+
                 } else {
-                    Toast.makeText(this@ShowTvDetailActivity, "Something broke ${response.message()}", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this@ShowTvDetailActivity,
+                        "Something broke ${response.message()}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
 
             override fun onFailure(call: Call<MySchedule.Show>, t: Throwable) {
-                Toast.makeText(this@ShowTvDetailActivity, "Something went wrong $t", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this@ShowTvDetailActivity,
+                    "Something went wrong $t",
+                    Toast.LENGTH_LONG
+                ).show()
             }
 
 
         })
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView!!.canGoBack()) {
+            this.webView!!.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event)
     }
 
     private fun loadShowCast(recyclerView: RecyclerView, showId: String){
@@ -105,7 +153,7 @@ class ShowTvDetailActivity : AppCompatActivity() {
 
         requestCall.enqueue(object : Callback<List<Cast>> {
             override fun onResponse(call: Call<List<Cast>>, response: Response<List<Cast>>) {
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     val castList = response.body()!!
                     recyclerView.apply {
                         setHasFixedSize(true)
@@ -113,12 +161,20 @@ class ShowTvDetailActivity : AppCompatActivity() {
                         adapter = CrewAdapter(castList)
                     }
                 } else {
-                    Toast.makeText(this@ShowTvDetailActivity, "Something went wrong ${response.message()}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@ShowTvDetailActivity,
+                        "Something went wrong ${response.message()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
             override fun onFailure(call: Call<List<Cast>>, t: Throwable) {
-                Toast.makeText(this@ShowTvDetailActivity, "Something went wrong ${t}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@ShowTvDetailActivity,
+                    "Something went wrong ${t}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
         })
